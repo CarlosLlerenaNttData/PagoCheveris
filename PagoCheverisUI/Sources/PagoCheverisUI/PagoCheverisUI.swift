f9e3e91bbf033800eb17c1de45b4ade945d95eee
Carlos Llerena <cllerenh@emeal.nttdata.com>
2022-06-22 22:33:43 -0500

    PagoCheverisUI Package aggregate

---
+++ PagoCheverisUI/Sources/PagoCheverisUI/PagoCheverisUI.swift
@@ -0,0 +1,14 @@
+//
+//  File.swift
+//  
+//
+//  Created by Carlos Alfredo Llerena Huayta on 22/06/22.
+//
+
+import Foundation
+
+public struct PagoCheverisUI {
+    public init(){
+        
+    }
+}
