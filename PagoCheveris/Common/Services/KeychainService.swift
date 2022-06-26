23eacf718e86202d395ebd3098fac21553368df4
Carlos Llerena <cllerenh@emeal.nttdata.com>
2022-06-26 17:27:05 -0500

    keychain Service created

---
+++ PagoCheveris/Common/Services/KeychainService.swift
@@ -0,0 +1,41 @@
+//
+//  KeychainService.swift
+//  PagoCheveris
+//
+//  Created by Carlos Alfredo Llerena Huayta on 26/06/22.
+//
+
+import KeychainAccess
+import Foundation
+
+protocol KeychainServiceProvider {
+    
+    func save(value: String, key: String)
+    func delete(key: String)
+    func value(for key: String) -> String?
+}
+
+final class KeychainService: KeychainServiceProvider {
+    
+    static let shared = KeychainService()
+    
+    private let keychain: Keychain
+    
+    private init() {
+        keychain = Keychain()
+    }
+
+    // MARK: Public methods
+    
+    func save(value: String, key: String) {
+        keychain[key] = value
+    }
+      
+    func delete(key: String) {
+        keychain[key] = nil
+    }
+    
+    func value(for key: String) -> String? {
+        keychain[key]
+    }
+}
