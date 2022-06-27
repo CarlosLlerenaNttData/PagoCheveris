df969580915a02c61fe44c089110d7c5490b48e4
Carlos Llerena <cllerenh@emeal.nttdata.com>
2022-06-26 22:15:01 -0500

    Revert "remove view controller"
    This reverts commit 65173294c84e0d37a06c6f02ee6023a8499bdd6b.

---
+++ PagoCheveris/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  PagoCheveris
+//
+//  Created by Carlos Alfredo Llerena Huayta on 16/06/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
