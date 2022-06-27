07007719f41250d0dc62542040c73c0d9af39cf0
Carlos Llerena <cllerenh@emeal.nttdata.com>
2022-06-26 22:54:57 -0500

    Regex and Strings

---
+++ PagoCheveris/Features/Login/Common/Entity/LoginRegex.swift
@@ -0,0 +1,12 @@
+//
+//  LoginRegex.swift
+//  PagoCheveris
+//
+//  Created by Carlos Alfredo Llerena Huayta on 26/06/22.
+//
+
+struct LoginRegex {
+    
+    static let username: String = "^.{1,10}$"
+    static let userPassword: String = "^.{1,30}$"
+}
---
+++ PagoCheveris/Features/Login/Common/Strings/LoginStrings.swift
@@ -0,0 +1,21 @@
+//
+//  LoginStrings.swift
+//  PagoCheveris
+//
+//  Created by Carlos Alfredo Llerena Huayta on 26/06/22.
+//
+
+struct LoginStrings {
+    
+    struct Login {
+        
+        static let facialRecognition = "Rec. Facial"
+        static let forgotPassword = "¿Olvidaste tu contraseña?"
+        static let password = "Contraseña"
+        static let rememberMe = "Recordar mis datos"
+        static let submit = "Inicia sesión"
+        static let subTitle = "¡Bienvenido de nuevo!"
+        static let title = "Pago Cheveris"
+        static let username = "Usuario"
+    }
+}
