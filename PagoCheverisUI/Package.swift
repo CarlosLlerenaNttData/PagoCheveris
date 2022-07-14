5beea801c49c6122b4cd76d899d730a097583c58
Carlos Llerena <cllerenh@emeal.nttdata.com>
2022-07-14 01:11:14 -0500

    Fixes Packages

--- PagoCheverisUI/Package.swift
+++ PagoCheverisUI/Package.swift
@@ -15,14 +15,16 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "https://github.com/airbnb/lottie-ios.git", from: Version(3, 3, 0))
+        .package(url: "https://github.com/airbnb/lottie-ios.git", from: Version(3, 3, 0)),
+        .package(url: "https://github.com/SnapKit/SnapKit.git", from: Version(5, 6, 0)),
+        .package(url: "https://github.com/slackhq/PanModal.git", from: Version(1, 2, 7)),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "PagoCheverisUI",
-            dependencies: [.product(name: "Lottie", package: "lottie-ios")],
+            dependencies: [.product(name: "Lottie", package: "lottie-ios"), "SnapKit","PanModal"],
             resources: [
                 .process("Resources/PCLogo.json")
             ]),
